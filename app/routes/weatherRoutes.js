ccb8387040d1b4b53d3084d8b76f2e0ac1aa9006
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-07 21:44:28 -0600

    Add route to get address by coordinates

--- app/routes/weatherRoutes.js
+++ app/routes/weatherRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 import {
   getWeatherByCity,
   getWeatherByCoordinates,
+  getAddressByCoordinates,
 } from '../controllers/weatherController.js'
 
 const router = express.Router()
 
 router.get('/:city', getWeatherByCity)
 router.get('/:latitude/:longitude', getWeatherByCoordinates)
+router.get('/coordinates/:latitude/:longitude', getAddressByCoordinates)
 
 export default router
