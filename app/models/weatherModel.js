34e8be49edd357d0988dd7c92b2300bfb46c9987
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-07 21:44:40 -0600

    Add getAddress function to retrieve address by coordinates

--- app/models/weatherModel.js
+++ app/models/weatherModel.js
@@ -1,6 +1,56 @@
 import { client } from '../config/redisClient.js'
 import axios from 'axios'
 
+export const getAddress = async (latitude, longitude) => {
+  try {
+    const cacheData = await client.get(`${latitude},${longitude}`)
+    if (cacheData) return JSON.parse(cacheData)
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.KEY_OPENCAGEDATA}`
+    const response = await fetch(url)
+    const data = await response.json()
+    const components = data.results[0].components
+    const road = components.road
+    const state = components.state
+    const county = components.county
+    const country = components.country
+    const postcode = components.postcode
+    const city = components._normalized_city
+    const residential = components.residential
+
+    await client.set(
+      `${latitude}_${longitude}`,
+      JSON.stringify({
+        road,
+        city,
+        state,
+        county,
+        country,
+        postcode,
+        latitude,
+        longitude,
+        residential,
+      }),
+      'EX',
+      3600
+    )
+
+    return {
+      road,
+      city,
+      state,
+      county,
+      country,
+      postcode,
+      latitude,
+      longitude,
+      residential,
+    }
+  } catch (error) {
+    console.error('Error al obtener los datos de ubicación:', error)
+    res.status(500).json({ error: 'Error fetching location data' })
+  }
+}
+
 export const getWeatherDataByCity = async (city) => {
   try {
     const cacheData = await client.get(city)
